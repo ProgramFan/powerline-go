2738d2aa84a0279ecd567db2f4db8337488daaaa
Yang Zhang <yang_zhang@iapcm.ac.cn>
2021-05-28 23:27:57 +0800

    Refactor segment-gitlite to use go-git

--- segment-gitlite.go
+++ segment-gitlite.go
@@ -1,76 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"os/exec"
-	"strings"
+	"github.com/go-git/go-git/v5"
 
 	pwl "github.com/justjanne/powerline-go/powerline"
 )
 
-var gitProcessEnv = func() []string {
-	homeEnv := homeEnvName()
-	home, _ := os.LookupEnv(homeEnv)
-	path, _ := os.LookupEnv("PATH")
-	env := map[string]string{
-		"LANG":  "C",
-		homeEnv: home,
-		"PATH":  path,
-	}
-	result := make([]string, 0)
-	for key, value := range env {
-		result = append(result, fmt.Sprintf("%s=%s", key, value))
-	}
-	return result
-}()
-
-func runGitCommand(cmd string, args ...string) (string, error) {
-	command := exec.Command(cmd, args...)
-	command.Env = gitProcessEnv
-	out, err := command.Output()
-	return string(out), err
-}
-
-func getGitDetachedBranch(p *powerline) string {
-	out, err := runGitCommand("git", "rev-parse", "--short", "HEAD")
-	if err != nil {
-		out, err := runGitCommand("git", "symbolic-ref", "--short", "HEAD")
-		if err != nil {
-			return "Error"
-		}
-		return strings.SplitN(out, "\n", 2)[0]
-	}
-	detachedRef := strings.SplitN(out, "\n", 2)
-	return fmt.Sprintf("%s %s", p.symbols.RepoDetached, detachedRef[0])
-}
-
 func segmentGitLite(p *powerline) []pwl.Segment {
-	if len(p.ignoreRepos) > 0 {
-		out, err := runGitCommand("git", "rev-parse", "--show-toplevel")
-		if err != nil {
-			return []pwl.Segment{}
-		}
-		out = strings.TrimSpace(out)
-		if p.ignoreRepos[out] {
-			return []pwl.Segment{}
-		}
-	}
-
-	out, err := runGitCommand("git", "rev-parse", "--abbrev-ref", "HEAD")
+	repo, err := git.PlainOpenWithOptions(p.cwd, &git.PlainOpenOptions{
+		DetectDotGit:          true,
+		EnableDotGitCommonDir: true,
+	})
 	if err != nil {
 		return []pwl.Segment{}
 	}
-
-	status := strings.TrimSpace(out)
-	var branch string
-
-	if status == "HEAD" {
-		branch = getGitDetachedBranch(p)
-	} else {
-		branch = status
+	if len(p.ignoreRepos) > 0 {
+		root := repoRoot(repo)
+		if p.ignoreRepos[root] {
+			return []pwl.Segment{}
+		}
 	}
 
+	branch := repoBranch(repo)
 	return []pwl.Segment{{
 		Name:       "git-branch",
 		Content:    branch,
